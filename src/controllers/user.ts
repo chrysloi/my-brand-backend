3ed3734c50ef6fc28b62e52911549ce705dc16a6
chrysanthe <apple@apples-MacBook-Pro.local>
2024-02-12 23:56:04 +0200

    ft: register && login

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,65 @@
 import { Request, Response } from "express";
+import { BAD_REQUEST, CREATED, OK, UNAUTHORIZED } from "http-status";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { user } from "../models/userModel";
+import env from "../util/envValidate";
 
 class Controller {
-  async register(req: Request, res: Response) {}
+  async register(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+
+    let hashedPassword = await bcrypt.hash(password, 10);
+    const users = await user.find({});
+    if (users.length === 0) {
+      await user.create({
+        name,
+        email,
+        password: hashedPassword,
+        role: "admin",
+        is_verified: true,
+      });
+      return res
+        .status(CREATED)
+        .json({ message: "Admin created Successfully" });
+    }
+    const userExisted = await user.findOne({ email });
+
+    if (userExisted)
+      return res.status(BAD_REQUEST).json({ message: "email already taken" });
+
+    await user.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: "user",
+      is_verified: true,
+    });
+
+    return res.status(CREATED).json({ message: "User created Successfully" });
+  }
+
+  async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const userExists = await user.findOne({ email });
+    if (!userExists)
+      return res.status(UNAUTHORIZED).json({ message: "User doesn't exists" });
+
+    const valid_pass = await bcrypt.compare(password, userExists.password);
+    if (!valid_pass)
+      return res
+        .status(UNAUTHORIZED)
+        .json({ message: "Password doesn't match" });
+
+    const token = jwt.sign({ userId: userExists.id }, env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    return res
+      .status(OK)
+      .json({ user: userExists, token, message: "Successfulll logged in" });
+  }
 }
 
 export const userController = new Controller();
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,5 +5,6 @@ import { userController } from "../controllers/user";
 const route = Router();
 
 route.post("/register", asyncHandler(userController.register));
+route.post("/login", asyncHandler(userController.login));
 
 export default route;
